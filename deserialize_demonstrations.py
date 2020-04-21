2a76cd2c9db4b1c684b5a39472f94a7bf16e52b8
Tommaso Bendinelli <tommaso.bendinelli@gmail.com>
2020-04-21 14:41:50 +0200

    Demonstration Ready

--- deserialize_demonstrations.py
+++ deserialize_demonstrations.py
@@ -4,7 +4,7 @@ from matplotlib.patches import RegularPolygon, FancyArrow
 #from .generalization_grid_game import create_gym_envs
 from generalization_grid_games.envs.playingXYZGeneralizationGridGame import PlayingXYZGeneralizationGridGame
 from generalization_grid_games.envs.utils import get_asset_path, changeResolution
-
+from copy import deepcopy
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from matplotlib.patches import RegularPolygon, FancyArrow
 import sys
@@ -16,36 +16,50 @@ import os
 
 EMPTY = [None,""]
 P = 'P'
+P_Clicked = 'P_highlighted'
 S = 'S'
+S_CLicked = 'S_highlighted'
 B = 'B'
 START = 's'
 PASS = 'pass'
+CLICK = 'click'
 ALL_TOKENS = [EMPTY, P, S, B, PASS, START]
-ALL_ACTION_TOKENS = [P, S, B, PASS, EMPTY]
+ALL_ACTION_TOKENS = [CLICK, PASS]
 
 TOKEN_IMAGES = {
     P: plt.imread(get_asset_path('p.png')),
     S: plt.imread(get_asset_path('s.png')),
     B: plt.imread(get_asset_path('b.png')),
     START: plt.imread(get_asset_path('start.png')),
+    P_Clicked: plt.imread(get_asset_path('P_highlighted.png')),
+    S_CLicked: plt.imread(get_asset_path('S_highlighted.png'))
 }
 
 def demonstration_as_dict_creator(data):
-    demonstration = dict()
+    demonstration = list()
     for time_step in range(len(data)):
-        demonstration[time_step] = data[time_step]
+        demonstration.append(data[time_step])
     return demonstration
 
 
 def find_difference(demonstration_seq):
-    diff = {key:[[None]*len(demonstration_seq[0][0]) for x in range(len(demonstration_seq[0]))] for key in demonstration_seq.keys()}
-    for delta in range(1,len(diff)):
-        for x in range(len(diff)):
-            for y in range(len(diff[0])):
-                if (demonstration_seq[delta][x][y] == demonstration_seq[delta-1][x][y]):
+    final_demo = deepcopy(demonstration_seq)
+    counter = 0 
+    #diff = [key:[[None]*len(demonstration_seq[0][0]) for x in range(len(demonstration_seq[0]))] for key in demonstration_seq.keys()}
+    for step in range(1,len(demonstration_seq)):
+        for x in range(len(demonstration_seq[0])):
+            for y in range(len(demonstration_seq[0][0])):
+                if (demonstration_seq[step][x][y] == demonstration_seq[step-1][x][y]):
                     continue
-                diff[delta] = "Old:" + demonstration_seq[delta-1][x][y] + "New:" + demonstration_seq[delta][x][y]
-    return diff
+                elif demonstration_seq[step-1][x][y] == P:
+                    final_demo.insert(step+counter,deepcopy(final_demo[step-1+counter]))
+                    final_demo[step+counter][x][y] =  "P_highlighted"
+                    counter = counter + 1
+                elif demonstration_seq[step-1][x][y] == S:
+                    final_demo.insert(step+counter,deepcopy(final_demo[step-1+counter]))
+                    final_demo[step+counter][x][y] =  "S_highlighted"
+                    counter = counter + 1
+    return final_demo
 
 
 
@@ -91,21 +105,22 @@ class Visualization():
     def on_key(self, event):
         self.to_next_timestep()
         print("here")
-        self.render_onscreen_t()
+        self.render_onscreen()
         self.fig.canvas.draw()
         return
         #plt.show()
-    def render_onscreen_t(self):
-        for drawing in self.drawings:
-            drawing.remove()
-        self.drawings = []
 
-        for r in range(self.height):
-            for c in range(self.width):
-                token = self.dict_seq[self.curr_time][r][c]
-                drawing = self.draw_token(token, r, c, self.ax, height = self.height,width=self.width)
-                if drawing is not None:
-                    self.drawings.append(drawing)
+    # def render_onscreen_t(self):
+    #     for drawing in self.drawings:
+    #         drawing.remove()
+    #     self.drawings = []
+
+    #     for r in range(self.height):
+    #         for c in range(self.width):
+    #             token = self.dict_seq[self.curr_time][r][c]
+    #             drawing = self.draw_token(token, r, c, self.ax, height = self.height,width=self.width)
+    #             if drawing is not None:
+    #                 self.drawings.append(drawing)
     
     # @staticmethod
     # def merge_multiple_images(token):
@@ -133,6 +148,8 @@ class Visualization():
 
         for r in range(self.height):
             for c in range(self.width):
+                if r == 20 and c == 21:
+                    print("hello")
                 token = self.dict_seq[self.curr_time][r][c]
                 drawing = self.draw_token(token, r, c, self.ax, height = self.height,width=self.width)
                 if drawing is not None:
@@ -165,7 +182,7 @@ class Visualization():
                                          fc=face_color)
             ax.add_patch(drawing)
 
-            tk = list(token.replace(" ",""))[-1]
+            tk = token.split()[-1]
             im = TOKEN_IMAGES[tk]
             oi = OffsetImage(im, zoom = 1 * (self.fig_scale*2  / max(height, width)**0.5))
             box = AnnotationBbox(oi, (c + 0.5, (height - 1 - r) + 0.5), frameon=False)
@@ -230,7 +247,8 @@ with open('unity_demonstrations/matrix.json') as json_file:
     data = json.load(json_file)
 
 dict_seq = demonstration_as_dict_creator(data)
-interaction = Visualization(dict_seq)
+final_demo = find_difference(dict_seq)
+interaction = Visualization(final_demo)
 interaction.visualize_demonstration()
 #output = concat_n_images("B P")
 #plt.imshow(output)
